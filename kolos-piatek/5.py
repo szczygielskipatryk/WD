a84f9fd428128a99ecd65c54d19ca57598fa8989
szczygielskipatryk <32666895+szczygielskipatryk@users.noreply.github.com>
2018-04-12 20:56:09 +0200

    Update 5.py

--- kolos-piatek/5.py
+++ kolos-piatek/5.py
@@ -20,6 +20,7 @@ l=n[2]
 prac1=k.split(",")
 prac2=i.split(",")
 prac3=l.split(",")
+plik.close()
 jan=Pracownik(prac1[0],prac1[1],prac1[2],prac1[3])
 jan.__str__()
 jan2=Pracownik(prac2[0],prac2[1],prac2[2],prac2[3])
